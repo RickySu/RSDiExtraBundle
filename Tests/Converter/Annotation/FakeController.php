40abc9091c5c381ae4d1608103aeaa42109569b6
Ricky Su <ricky@ez2.us>
2018-07-13 14:40:56 +0800

    fix property injection

---
+++ Tests/Converter/Annotation/FakeController.php
@@ -0,0 +1,11 @@
+<?php
+namespace RS\DiExtraBundle\Tests\Converter\Annotation;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FakeController extends Controller
+{
+    protected $property;
+
+}
