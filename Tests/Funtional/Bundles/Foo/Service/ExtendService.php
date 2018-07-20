15596b1de7b977e7ce93f105733db2bec0e97b32
Ricky Su <ricky@ez2.us>
2018-07-20 11:49:08 +0800

    add extend service override test

---
+++ Tests/Funtional/Bundles/Foo/Service/ExtendService.php
@@ -0,0 +1,44 @@
+<?php
+namespace RS\DiExtraBundle\Tests\Funtional\Bundles\Foo\Service;
+
+use RS\DiExtraBundle\Annotation\Inject;
+use RS\DiExtraBundle\Annotation\InjectParams;
+use RS\DiExtraBundle\Annotation\Service;
+
+/**
+ * @Service("extend_service")
+ */
+class ExtendService extends AbstractService
+{
+    public $fooNotPublicService;
+
+    /**
+     * @InjectParams()
+     */
+    public function injectFooNotPublicService(FooNotPublicService $fooNotPublicService)
+    {
+        $this->fooNotPublicService = $fooNotPublicService;
+    }
+
+    /**
+     * @param $fooNotPublicService
+     * @InjectParams({
+     *     "fooNotPublicService" = @Inject("foo_not_public")
+     * })
+     */
+    public function injectFooService($fooNotPublicService)
+    {
+        $this->foo = $fooNotPublicService;
+    }
+
+    /**
+     * @param $fooNotPublicService
+     * @InjectParams({
+     *     "fooNotPublicService" = @Inject("foo_not_public")
+     * })
+     */
+    public function injectFoo2Service($fooNotPublicService)
+    {
+        $this->foo2 = $fooNotPublicService;
+    }
+}
