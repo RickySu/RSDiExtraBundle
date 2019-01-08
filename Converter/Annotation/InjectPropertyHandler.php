eed44bd052010813c78b7bcba5f892cf8d7c5c39
Ricky Su <ricky@ez2.us>
2019-01-08 12:35:39 +0800

    add AbstractController instalce detect

--- Converter/Annotation/InjectPropertyHandler.php
+++ Converter/Annotation/InjectPropertyHandler.php
@@ -3,6 +3,7 @@ namespace RS\DiExtraBundle\Converter\Annotation;
 
 use RS\DiExtraBundle\Annotation\Inject;
 use RS\DiExtraBundle\Converter\ClassMeta;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class InjectPropertyHandler
@@ -25,7 +26,7 @@ class InjectPropertyHandler
             $classMeta->id = $classMeta->class;
         }
 
-        if($reflectionProperty->getDeclaringClass()->isSubclassOf(Controller::class)){
+        if($this->isController($reflectionProperty->getDeclaringClass())){
             if($annotation->value === null){
                 $classMeta->controllerProperties[$reflectionProperty->getName()] = $this->parameterGuesser->guessArgument($reflectionProperty->getName());
                 return;
@@ -42,5 +43,18 @@ class InjectPropertyHandler
 
         $classMeta->properties[$reflectionProperty->getName()] = $this->parameterGuesser->guessAnnotationArgument($annotation->value, $annotation->required);
 
+    }
+
+    protected function isController(\ReflectionClass $reflectionClass)
+    {
+        if($reflectionClass->isSubclassOf(Controller::class)){
+            return true;
+        }
+
+        if($reflectionClass->isSubclassOf(AbstractController::class)){
+            return true;
+        }
+
+        return false;
     }
 }
--- Tests/Funtional/ControllerTest.php
+++ Tests/Funtional/ControllerTest.php
@@ -27,4 +27,27 @@ class ControllerTest extends BaseWebTestCase
         $this->assertEquals('bar', $result['constructParams']['foo']);
 
     }
+
+    public function test_property_inject_abstract_controller_instance()
+    {
+        //arrange
+
+        //act
+        $this->client->request('GET', "/foo2/bar");
+        $result = json_decode($this->client->getResponse()->getContent(), true);
+
+        //assert
+        $this->assertEquals('foo_static_factory', $result['fooStaticFactory']['params']['id']);
+        $this->assertEquals('foo_static_factory2', $result['fooStaticFactory2']['params']['id']);
+        $this->assertEquals('foo_static_factory3', $result['injectParams']['fooStaticFactory3']['params']['id']);
+        $this->assertEquals('bar', $result['injectParams']['foo']);
+        $this->assertEquals('foo_static_factory', $result['injectParams1']['fooStaticFactory']['params']['id']);
+        $this->assertEquals('foo_static_factory2', $result['injectParams1']['fooStaticFactory2']['params']['id']);
+        $this->assertEquals('foo_static_factory3', $result['injectParams1']['fooStaticFactory3']['params']['id']);
+        $this->assertEquals('bar', $result['injectParams1']['foo']);
+        $this->assertEquals('foo_static_factory2', $result['constructParams']['fooStaticFactory2']['params']['id']);
+        $this->assertEquals('foo_static_factory3', $result['constructParams']['fooStaticFactory3']['params']['id']);
+        $this->assertEquals('bar', $result['constructParams']['foo']);
+
+    }
 }
